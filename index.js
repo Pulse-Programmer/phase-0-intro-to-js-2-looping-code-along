fdf2e6624e6c40c7871895e5f9136fbc41c3cd50
Stephen Ochieng <materiamystic@gmail.com>
2024-03-22 00:15:48 +0300

    Adds required functions

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+function writeCards(arr, eventName) {
+  const newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    let message = `Thank you, ${arr[i]}, for the wonderful ${eventName} gift!`;
+    newArr.push(message);
+  }
+  return newArr;
+}
+
+function countDown(j) {
+  j = Math.floor(j);
+  if (j > 0) {
+    while (j !== -1) {
+      console.log(j--);
+    }
+  } else console.error("Error: Enter positive integer!");
+}
